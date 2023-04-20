7a73f05df04122c97f0962ea782acb2ddfd180dd
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-04-20 14:08:42 +0200

    delete cors

--- backend/app.js
+++ backend/app.js
@@ -22,11 +22,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 module.exports = app;
