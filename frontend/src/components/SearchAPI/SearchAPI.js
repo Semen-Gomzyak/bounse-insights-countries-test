64f6aff8a5d888dde1f8286190b835714c49b6b6
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-04-22 16:29:20 +0200

    Update SearchAPI.js

--- frontend/src/components/SearchAPI/SearchAPI.js
+++ frontend/src/components/SearchAPI/SearchAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/';
+const BASE_URL = 'https://countries-test.onrender.com:3001/';
 
 // BASE_URL = 'https://frabjous-pudding-cfcda6.netlify.app/
 
