6b3d637b53bac74209986b68a47d0b9337e8e789
Semen-Gomzyak <Blacklife1817@gmail.com>
2023-04-23 15:08:06 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://frabjous-pudding-cfcda6.netlify.app',
+  );
+  res.header('Access-Control-Allow-Credentials', true);
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
